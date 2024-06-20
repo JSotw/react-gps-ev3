a1bfe122a6270fd466dcd1271c19adaefe8a3f29
JSotw <janoguerrasks@gmail.com>
2024-06-20 14:58:08 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
 import "./App.css";
+import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
 import { get, ref } from "firebase/database";
 import { db } from "./firebase/config.js";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { MapContainer, TileLayer, Popup, Marker, Circle } from "react-leaflet";
+import { Icon } from "leaflet";
+import gpsIcon from "./assets/icon/gps-icon.png";
 
 function App() {
   const [locations, setLocations] = useState([]);
   const [users, setUsers] = useState([]);
   const [userId, setUserId] = useState("");
   const [loading, setLoading] = useState(true);
+  const [position, setPosition] = useState();
+  const [map, setMap] = useState(null);
 
-  let userLocationData = "";
-  let userData = "";
-
-  useEffect(() => {
-    setInterval(() => {
-      getUsersData();
-      getLocationData();
-      setLoading(false);
-    }, 1000);
-  }, []);
-  // console.log(users);
-  // console.log(userId);
+  let centerOut = [-35.4364, -71.64442];
 
   const getLocationData = async () => {
     const locationReef = ref(db, "location");
@@ -66,6 +61,93 @@ function App() {
       });
   };
 
+  useEffect(() => {
+    getUsersData();
+    getLocationData();
+    // Intervalo para actualizar los datos cada 6segundos
+    if (users != undefined && locations != undefined) {
+      setInterval(() => {
+        getUsersData();
+        getLocationData();
+      }, 6000);
+    }
+    setInterval(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+  // console.log(users);
+  // console.log(userId);
+
+  let userData = users?.find((user) => user.id === userId);
+  // let position = {
+  //   lat: userLocationData?.latitude,
+  //   lng: userLocationData?.longitude,
+  // };
+
+  //console.log(mapCenter);
+  let pos = locations?.find((location) => location.id === userId);
+  let actPos = {
+    lat: pos?.latitude,
+    lng: pos?.longitude,
+  };
+
+  const onChange = (e) => {
+    // Dividir la cadena en dos partes usando split(',')
+    const valuesArray = e.split(",");
+    // Crear un objeto con las dos partes
+    const valuesObject = {
+      id: valuesArray[0],
+      lat: valuesArray[1],
+      lng: valuesArray[2],
+    };
+    let latLng = {
+      lat: valuesObject.lat,
+      lng: valuesObject.lng,
+    };
+    //console.log(coordenadasObjeto.id);
+    setUserId(valuesObject.id);
+    if (locations) {
+      setPosition(latLng);
+      map.setView(latLng, map.getZoom());
+    }
+    
+  };
+
+  // Definir los límites del rango de coordenadas
+  const bounds = {
+    north: -35.4264, // 0.01 grados al norte
+    south: -35.4464, // 0.01 grados al sur
+    east: -71.6076, // 0.01 grados al este
+    west: -71.6276, // 0.01 grados al oeste
+  };
+
+  // Función para verificar si la posición está dentro de los límites
+  const isWithinBounds = ({ lat, lng }) => {
+    return (
+      lat <= bounds.north &&
+      lat >= bounds.south &&
+      lng <= bounds.east &&
+      lng >= bounds.west
+    );
+  };
+
+  useEffect(() => {
+    if (position) {
+      if (!isWithinBounds(position)) {
+        alert("Estás fuera del rango permitido");
+        // Aquí puedes agregar lógica adicional si la posición está fuera del rango
+      }
+    }
+  }, [position]);
+
+  console.log(position);
+
+  const customIcon = new Icon({
+    iconUrl: gpsIcon,
+    iconSize: [40, 42],
+    shadowSize: [41, 41],
+  });
+
   const zoom = 17;
   if (!loading) {
     if (userId === "") {
@@ -79,79 +161,106 @@ function App() {
             className="w-full h-10 px-3 text-center font-semibold rounded-lg"
             name=""
             id=""
-            onChange={(e) => setUserId(e.target.value)}
+            onChange={(e) => onChange(e.target.value)}
           >
-            <option selected={true} disabled={true} value="">
+            <option selected={true} value="">
               Seleccione un usuario...
             </option>
-            {users.map((user) => (
-              <option key={user.id} value={user.id}>
-                {user.nombre}
+            {locations?.map((location) => (
+              <option
+                key={location.id}
+                value={[location.id, location.latitude, location.longitude]}
+              >
+                {users?.map((user) =>
+                  user.id === location.userId ? user.nombre : ""
+                )}
               </option>
             ))}
           </select>
         </section>
       );
     } else {
-      //console.log("User selected");
-      userData = users.find((user) => user.id === userId);
-      userLocationData = locations.find(
-        (location) => location.userId === userId
-      );
-      //console.log(userLocationData);
-
+      const fillBlueOptions = { fillColor: "blue" };
+      const purpleOptions = { color: "red" };
       return (
         <main>
           <h1 className="font-bold mb-6">Ubicación</h1>
           <div>
             {
-              <section
-                className="flex flex-row gap-3 rounded-lg"
-              >
-                <div
-                  className="bg-white rounded-lg p-6 text-left text-gray-700 w-56"
-                >
-                  <h4 className="font-semibold">Información adicional</h4>
-                  <ul className="text-left">
-                    <li>{`${userData?.nombre} ${
-                      userData?.apellido ? userData?.apellido : ""
-                    } ${
-                      userData?.apellidoMaterno ? userData?.apellidoMaterno : ""
-                    }`}</li>
-                    <li>{userData?.email}</li>
-                    <li>{userData?.telefono}</li> 
+              <section className="flex flex-row gap-3 rounded-lg">
+                <div className="bg-white rounded-lg p-5 text-left text-gray-700 w-64">
+                  <h4 className="font-bold text-[17px] mb-2">
+                    Información adicional
+                  </h4>
+                  <ul className="text-left text-sm">
+                    <li>
+                      <b>Nombre:{" "}</b>
+                      {`${userData?.nombre} ${
+                        userData?.apellido ? userData?.apellido : ""
+                      } ${
+                        userData?.apellidoMaterno
+                          ? userData?.apellidoMaterno
+                          : ""
+                      }`}
+                    </li>
+                    <li><b>Correo: </b> {userData?.email}</li>
+                    <li><b>Teléfono: </b>{userData?.telefono}</li>
                   </ul>
                 </div>
                 <section className="flex flex-col gap-3">
-                  <div
-                    className="rounded-lg"
-                  >
-                    <iframe
-                      src={`https://maps.google.com/maps?q=${userLocationData?.latitude},${userLocationData?.longitude}&z=${zoom}&output=embed`}
-                      width="600"
-                      height="450"
-                      allowFullScreen
-                      loading="lazy"
-                      referrerPolicy="no-referrer-when-downgrade"
-                      className="rounded-lg"
-                    ></iframe>
+                  <div className="rounded-lg">
+                    <MapContainer
+                      ref={setMap}
+                      center={position}
+                      zoom={zoom}
+                      scrollWheelZoom={true}
+                      className="rounded-lg h-96"
+                    >
+                      <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                      />
+                      <Circle
+                        center={centerOut}
+                        pathOptions={fillBlueOptions}
+                        radius={5000}
+                      />
+                      <Circle
+                        center={position}
+                        pathOptions={purpleOptions}
+                        radius={150}
+                      />
+                      <Marker icon={customIcon} position={position} isActive>
+                        <Popup isActive>
+                          <p>{userData?.nombre}{" "}
+                          {userData?.apellido ? userData?.apellido : ""}</p>
+                          <p>Latitud: {position.lat}</p>
+                          <p>Longitud: {position.lng}</p>
+                        </Popup>
+                      </Marker>
+                    </MapContainer>
                   </div>
                   <select
                     className="w-full h-10 px-3 text-center font-semibold rounded-lg"
                     name=""
                     id=""
-                    onChange={(e) => setUserId(e.target.value)}
+                    onChange={(e) => onChange(e.target.value)}
                   >
-                    <option selected={true} disabled={true} value="">
+                    <option selected={true} value="">
                       Seleccione un usuario...
                     </option>
-                    {users.map((user) => (
+                    {locations?.map((location) => (
                       <option
-                        style={{ height: "50px" }}
-                        key={user.id}
-                        value={user.id}
+                        key={location.id}
+                        value={[
+                          location.id,
+                          location.latitude,
+                          location.longitude,
+                        ]}
                       >
-                        {user.nombre}
+                        {users?.map((user) =>
+                          user.id === location.userId ? user.nombre : ""
+                        )}
                       </option>
                     ))}
                   </select>
